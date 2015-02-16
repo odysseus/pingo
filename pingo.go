9b54404a05d9d28ff451648d4ab0d8f8559ec7ed
Ryan Case <rmcase@gmail.com>
2015-02-16 10:51:43 -0700

    Getting proper ping response from remote server

--- pingo.go
+++ pingo.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"errors"
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 const (
-	ICMP_TYPE = 8 // 8 = Echo request
-	ICMP_CODE = 0 // 0 = Echo code
+	ICMP_ECHO_REQUEST = 8 // Echo request code
+	ICMP_ECHO_REPLY   = 0 // Echo reply code
 )
 
 func main() {
@@ -24,14 +25,32 @@ func main() {
 	conn, err := net.DialIP("ip4:icmp", nil, raddr)
 	checkErr(err)
 
-	pid := uint16(os.Getpid())
-	var seq uint16 = 1
+	id := os.Getpid() & 0xffff
+	seq := 0
+	pktlen := 32
 
-	p, err := makePacket(pid, seq, []byte("Hello, world!"))
-	checkErr(err)
+	for {
+		seq++
+		p := makePacket(id, seq, pktlen, []byte("Hello, world!"))
+		checkErr(err)
 
-	fmt.Println(conn)
-	fmt.Println(p)
+		n, err := conn.Write(p)
+		if err != nil || n != len(p) {
+			log.Fatal("Packet send failed")
+		}
+
+		conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
+
+		resp := make([]byte, 1024)
+		for {
+			rlen, _, err := conn.ReadFrom(resp)
+			checkErr(err)
+
+			fmt.Println(resp[:rlen])
+			break
+		}
+		time.Sleep(1e9)
+	}
 }
 
 func checkErr(err error) {
@@ -40,17 +59,13 @@ func checkErr(err error) {
 	}
 }
 
-func makePacket(id, seq uint16, payload []byte) ([]byte, error) {
-	// Find the payload length and ensure it does not exceed the max
-	paylen := len(payload)
-	if paylen > 256 {
-		return nil, errors.New("Payload cannot exceed 256 bytes")
-	}
+func makePacket(id, seq, pktlen int, filler []byte) []byte {
+	p := make([]byte, pktlen)
+	copy(p[8:], bytes.Repeat(filler, (pktlen-8)/len(filler)+1))
 
 	// Set up the packet
-	p := make([]byte, 8+paylen)
-	p[0] = ICMP_TYPE         // Type
-	p[1] = ICMP_CODE         // Code
+	p[0] = ICMP_ECHO_REQUEST // Type
+	p[1] = 0                 // Code
 	p[2] = 0                 // Header checksum - set later
 	p[3] = 0                 // Header checksum - set later
 	p[4] = uint8(id >> 8)    // Identifier
@@ -58,34 +73,22 @@ func makePacket(id, seq uint16, payload []byte) ([]byte, error) {
 	p[6] = uint8(seq >> 8)   // Sequence number
 	p[7] = uint8(seq & 0xff) // Sequence number
 
-	// Calculate and add the checksum
-	c := checksum(p)
-	p[2] = uint8(c >> 8)
-	p[3] = uint8(c & 0xff)
-
-	// Add the payload
-	for i := 0; i < paylen; i++ {
-		p[8+i] = payload[i]
-	}
-
-	return p, nil
-}
-
-func checksum(p []byte) uint16 {
-	// http://www.faqs.org/rfcs/rfc1071.html
+	// calculate icmp checksum
 	cklen := len(p)
 	s := uint32(0)
-
 	for i := 0; i < (cklen - 1); i += 2 {
 		s += uint32(p[i+1])<<8 | uint32(p[i])
 	}
-
 	if cklen&1 == 1 {
 		s += uint32(p[cklen-1])
 	}
-
 	s = (s >> 16) + (s & 0xffff)
 	s = s + (s >> 16)
 
-	return uint16(s)
+	// place checksum back in header; using ^= avoids the
+	// assumption the checksum bytes are zero
+	p[2] ^= uint8(^s & 0xff)
+	p[3] ^= uint8(^s >> 8)
+
+	return p
 }
